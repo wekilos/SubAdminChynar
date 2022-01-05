1440570c607a64e1c881d375b6a105a13e856296
wekilos <wekilyazgulyyew2000@gmail.com>
2022-01-05 21:46:39 +0500

    deploy

--- src1/App.js
+++
@@ -1,9 +0,0 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import Routes from "./routes/routes";
-function App() {
-  return <Routes />;
-}
-
-export default App;
--- src1/utils/token.js
+++
@@ -1,7 +0,0 @@
-export const token = () => {
-    if (JSON.parse(localStorage.getItem("SubProfile"))) {
-      var data = JSON.parse(localStorage.getItem("SubProfile"));
-      return data.token;
-    }
-  };
-  
